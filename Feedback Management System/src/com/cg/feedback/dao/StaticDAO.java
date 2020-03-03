e1df740b356468bda9248fb7d3ddd2cfcf0fef91
ogupta261 <ogupta261@gmail.com>
2020-03-03 10:52:00 +0530

    StaticDAO complete.

--- Feedback Management System/src/com/cg/feedback/dao/StaticDAO.java
+++ Feedback Management System/src/com/cg/feedback/dao/StaticDAO.java
@@ -1,5 +1,47 @@
 package com.cg.feedback.dao;
 
-public interface StaticDAO {
+import java.util.HashMap;
+import java.util.Map;
 
+import com.cg.feedback.dto.*;
+
+public final class StaticDAO {
+	
+	private Map<Integer, TrainerDTO> trainers = new HashMap<Integer, TrainerDTO>();
+	private Map<Integer, StudentDTO> students = new HashMap<Integer, StudentDTO>();
+	private Map<Integer, ProgramDTO> programs = new HashMap<Integer, ProgramDTO>();
+	private Map<Integer, CourseDTO> courses = new HashMap<Integer, CourseDTO>();
+	private Map<Integer, FeedbackDTO> feedback = new HashMap<Integer, FeedbackDTO>();
+	
+	public final Map<Integer, TrainerDTO> getTrainers() {
+		return trainers;
+	}
+	public final void setTrainers(Map<Integer, TrainerDTO> trainers) {
+		this.trainers = trainers;
+	}
+	public final Map<Integer, StudentDTO> getStudents() {
+		return students;
+	}
+	public final void setStudents(Map<Integer, StudentDTO> students) {
+		this.students = students;
+	}
+	public final Map<Integer, ProgramDTO> getPrograms() {
+		return programs;
+	}
+	public final void setPrograms(Map<Integer, ProgramDTO> programs) {
+		this.programs = programs;
+	}
+	public final Map<Integer, CourseDTO> getCourses() {
+		return courses;
+	}
+	public final void setCourses(Map<Integer, CourseDTO> courses) {
+		this.courses = courses;
+	}
+	public final Map<Integer, FeedbackDTO> getFeedback() {
+		return feedback;
+	}
+	public final void setFeedback(Map<Integer, FeedbackDTO> feedback) {
+		this.feedback = feedback;
+	}
+	
 }
