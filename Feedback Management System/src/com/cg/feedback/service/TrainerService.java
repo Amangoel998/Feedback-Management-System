d0946a91527e7d174666a4e29b55669239a0ef5d
Aman Goel <amangoel998@gmail.com>
2020-03-17 01:39:45 +0530

    Added initial Projects

--- Feedback Management System/src/com/cg/feedback/service/TrainerService.java
+++
@@ -1,8 +0,0 @@
-package com.cg.feedback.service;
-
-import com.cg.feedback.exceptions.CustomException;
-
-public interface TrainerService extends UserService{
-	String showFeedBack() throws CustomException;
-	String showDefaulters() throws CustomException;
-}
